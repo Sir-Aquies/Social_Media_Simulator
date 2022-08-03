7e5a82977523ea5882b5193dd7acb2a7f7841944
JECT <eliank500@gmail.com>
2022-08-03 15:16:56 -0500

    MVD added, now i only need to learn how to use it.

---
+++ WebProject/Controllers/Profile.cs
@@ -0,0 +1,42 @@
+﻿#nullable disable
+using Microsoft.AspNetCore.Mvc;
+using WebProject.Models;
+using WebProject.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebProject.Controllers
+{
+    public class Profile : Controller
+    {
+        private readonly WebProjectContext _Models;
+
+        public Profile(WebProjectContext Models)
+        {
+            _Models = Models;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult UserPage(int? userId)
+        {
+            UserModel userModel = new UserModel();
+
+            if (userId == null)
+            {
+                return NotFound();
+            }
+
+            userModel = _Models.Users.Include(u => u.Posts).AsNoTracking().FirstOrDefault(us => us.Id == userId);
+
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userModel);
+        }
+    }
+}
