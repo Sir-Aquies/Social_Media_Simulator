955d4cbdadbe7a86051978c2f90e855eea32ddfa
JECT <eliank500@gmail.com>
2022-08-23 16:17:42 -0500

    EditProfile page finished? including validation (for the most part).

--- WebProject/Controllers/SettingsController.cs
+++ WebProject/Controllers/SettingsController.cs
@@ -24,12 +24,57 @@ namespace WebProject.Controllers
                 return View(null);
             }
 
-            userModel = await _Models.Users.Include(u => u.Posts).AsNoTracking().FirstOrDefaultAsync(us => us.Id == UserId);
+            userModel = await _Models.Users.AsNoTracking().FirstOrDefaultAsync(us => us.Id == UserId);
 
             if (userModel == null)
             {
                 return NotFound();
             }
+
+            return View(userModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditProfile(UserModel ChangeUser, IFormFile ProfilePicture)
+        {
+            UserModel userModel = new UserModel();
+
+            userModel = await _Models.Users.AsNoTracking().FirstOrDefaultAsync(us => us.Id == ChangeUser.Id);
+
+            if (userModel == null)
+            {
+                ViewBag.ErrorMessage = "Sorry, something went wrong.";
+                return View(userModel);
+            }
+
+            if (ChangeUser == null)
+            {
+                ViewBag.ErrorMessage = "Sorry, something went wrong.";
+                return View(userModel);
+            }
+
+            userModel.Username = ChangeUser.Username;
+            userModel.FirstName = ChangeUser.FirstName;
+            userModel.LastName = ChangeUser.LastName;
+            userModel.Description = ChangeUser.Description;
+
+            if (ProfilePicture != null)
+            {
+                userModel.ProfilePicture = await GetBytes(ProfilePicture);
+            }
+
+            _Models.Attach(userModel).State = EntityState.Modified;
+            await _Models.SaveChangesAsync();
+            ViewBag.Message = "Profile sucesfully updated.";
+
+            //if (await TryUpdateModelAsync<UserModel>(
+            //    userModel, "ChangeUser", u => u.Username, u => u.FirstName, u => u.FirstName, u => u.LastName, u => u.Description, u => u.ProfilePicture))
+            //{
+            //    _Models.Attach(userModel).State = EntityState.Modified;
+            //    await _Models.SaveChangesAsync();
+            //    ViewBag["Message"] = "Profile sucesfully updated.";
+            //    return View(userModel);
+            //}
 
             return View(userModel);
         }
@@ -51,6 +96,13 @@ namespace WebProject.Controllers
             }
 
             return View(userModel);
+        }
+
+        private async Task<byte[]> GetBytes(IFormFile formFile)
+        {
+            await using var memoryStream = new MemoryStream();
+            await formFile.CopyToAsync(memoryStream);
+            return memoryStream.ToArray();
         }
     }
 }
