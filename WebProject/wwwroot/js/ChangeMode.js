a9104ad932d7a54e9845df4a838accd1bd9477e3
JECT <eliank500@gmail.com>
2022-08-24 17:10:34 -0500

    Appearance view added and in constuction.

---
+++ WebProject/wwwroot/js/ChangeMode.js
@@ -0,0 +1,24 @@
+﻿function ChangeMode() {
+    const radioButtons = document.querySelectorAll('input[name="mode"]');
+
+    let selectedMode = "";
+
+    for (const mode of radioButtons) {
+        if (mode.checked) {
+            selectedMode = mode.value;
+            break;
+        }
+    }
+
+    if (selectedMode === "Lightmode") {
+        document.documentElement.style.setProperty('--BackgroundColor', 'white');
+        document.documentElement.style.setProperty('--LayoutColor', 'rgb(242, 242, 242)');
+        document.documentElement.style.setProperty('--colortext', 'black');
+    }
+
+    if (selectedMode === "Darkmode") {
+        document.documentElement.style.setProperty('--BackgroundColor', 'rgb(18, 18, 18)');
+        document.documentElement.style.setProperty('--LayoutColor', 'rgb(15, 15, 15)');
+        document.documentElement.style.setProperty('--colortext', 'white');
+    }
+}
