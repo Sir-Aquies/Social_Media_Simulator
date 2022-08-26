4b84a34c88494463500e9b76922df6c9351507ed
JECT <eliank500@gmail.com>
2022-08-26 14:53:03 -0500

    Costum Appearance added.

--- WebProject/wwwroot/js/ChangeMode.js
+++ WebProject/wwwroot/js/ChangeMode.js
@@ -1,6 +1,17 @@
-﻿const savedTheme = localStorage.getItem('Mode');
+﻿const UseCostum = localStorage.getItem('UseCostum');
 
-if (savedTheme) {
+if (UseCostum === "true") {
+    const SavedCostum = [];
+    SavedCostum[0] = localStorage.getItem('ColorBackground');
+    SavedCostum[1] = localStorage.getItem('ColorLayout');   
+    SavedCostum[2] = localStorage.getItem('ColorText');
+    SavedCostum[3] = localStorage.getItem('ColorBorder');
+    SavedCostum[4] = localStorage.getItem('ColorShadow');
+    SetAppearance(SavedCostum[0], SavedCostum[1], SavedCostum[2], SavedCostum[3], SavedCostum[4]);
+}
+
+const savedTheme = localStorage.getItem('Mode');
+if (savedTheme && UseCostum === "false") {
     SetMode(savedTheme);
 }
 
@@ -27,8 +38,31 @@ function ChangeMode() {
     //if (selectedMode === "Darkmode") {
 
     //}
+}
 
-    
+function CostumAppareance() {
+    const ColorBackground = document.getElementById("ColorBackground").value;
+    const ColorLayout = document.getElementById("ColorLayout").value;
+    const ColorText = document.getElementById("ColorText").value;
+    const ColorBorder = document.getElementById("ColorBorder").value;
+    const ColorShadow = document.getElementById("ColorShadow").value;
+
+    SetAppearance(ColorBackground, ColorLayout, ColorText, ColorBorder, ColorShadow);
+}
+
+function SetAppearance(ColorBackground, ColorLayout, ColorText, ColorBorder, ColorShadow) {
+    document.documentElement.style.setProperty('--BackgroundColor', ColorBackground);
+    document.documentElement.style.setProperty('--LayoutColor', ColorLayout);
+    document.documentElement.style.setProperty('--colortext', ColorText);
+    document.documentElement.style.setProperty('--BorderColor', ColorBorder);
+    document.documentElement.style.setProperty('--ShadowColor', ColorShadow);
+
+    localStorage.setItem("ColorBackground", ColorBackground);
+    localStorage.setItem("ColorLayout", ColorLayout);
+    localStorage.setItem("ColorText", ColorText);
+    localStorage.setItem("ColorBorder", ColorBorder);
+    localStorage.setItem("ColorShadow", ColorShadow);
+    localStorage.setItem("UseCostum", true);
 }
 
 function SetMode(mode) {
@@ -39,6 +73,7 @@ function SetMode(mode) {
         document.documentElement.style.setProperty('--BorderColor', 'rgb(0, 0, 0)');
         document.documentElement.style.setProperty('--colortext', 'black');
         localStorage.setItem("Mode", "Lightmode");
+        localStorage.setItem("UseCostum", false);
     }
 
     if (mode === "Darkmode") {
@@ -48,5 +83,6 @@ function SetMode(mode) {
         document.documentElement.style.setProperty('--BorderColor', 'rgb(255, 255, 255)');
         document.documentElement.style.setProperty('--colortext', 'white');
         localStorage.setItem("Mode", "Darkmode");
+        localStorage.setItem("UseCostum", false);
     }
 }
