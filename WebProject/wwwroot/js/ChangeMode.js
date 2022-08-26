854e723b8464ca53637b00c5eab8d4b4c7d16e3a
JECT <eliank500@gmail.com>
2022-08-26 13:14:49 -0500

    still struggling to add dark mode. Post Profile picture added.

--- WebProject/wwwroot/js/ChangeMode.js
+++ WebProject/wwwroot/js/ChangeMode.js
@@ -10,15 +10,28 @@
         }
     }
 
+    //if (selectedMode === "Lightmode") {
+    //    var sheet = document.styleSheets[0];
+    //    sheet.insertRule(":root{--BackgroundColor: rgb(255, 255, 255); --LayoutColor: rgb(253, 253, 253); --ShadowColor: rgb(110, 110, 110); --BorderColor: rgb(0, 0, 0); --colortext: black}");
+    //}
+
+    //if (selectedMode === "Darkmode") {
+
+    //}
+
     if (selectedMode === "Lightmode") {
-        document.documentElement.style.setProperty('--BackgroundColor', 'white');
-        document.documentElement.style.setProperty('--LayoutColor', 'rgb(242, 242, 242)');
+        document.documentElement.style.setProperty('--BackgroundColor', 'rgb(255, 255, 255)');
+        document.documentElement.style.setProperty('--LayoutColor', 'rgb(253, 253, 253)');
+        document.documentElement.style.setProperty('--ShadowColor', 'rgb(110, 110, 110)');
+        document.documentElement.style.setProperty('--BorderColor', 'rgb(0, 0, 0)');
         document.documentElement.style.setProperty('--colortext', 'black');
     }
 
     if (selectedMode === "Darkmode") {
         document.documentElement.style.setProperty('--BackgroundColor', 'rgb(18, 18, 18)');
         document.documentElement.style.setProperty('--LayoutColor', 'rgb(15, 15, 15)');
+        document.documentElement.style.setProperty('--ShadowColor', 'rgb(27, 27, 27)');
+        document.documentElement.style.setProperty('--BorderColor', 'rgb(255, 255, 255)');
         document.documentElement.style.setProperty('--colortext', 'white');
     }
 }
